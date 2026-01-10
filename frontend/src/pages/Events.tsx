47d9781a42206f2345dfae70ee9e2a4b6c6b7c4a
Jalaluddin Shaikh <jalaluddin.shaikh@sunware.in>
2026-01-10 15:32:14 +0530

    code refactor

--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -1,12 +1,20 @@
 import React, { type JSX } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 import type { Data } from "../utils/data-types";
 
 const EventsPage: React.FC = (): JSX.Element => {
+  const navigation = useNavigation();
+  const isLoading: boolean = navigation.state === "loading";
   const { events } = useLoaderData<Data>();
-  return <EventsList events={events} />;
+  let element: JSX.Element;
+  if (isLoading) {
+    element = <p className="loader">Loading...</p>;
+  } else {
+    element = <EventsList events={events} />;
+  }
+  return element;
 };
 
 export default EventsPage;
